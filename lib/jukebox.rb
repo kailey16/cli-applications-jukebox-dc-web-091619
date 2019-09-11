059481234d76ea444b680f9e7ee07bed604cc1e2
Kyeonga(Kailey) Lee <klee.mcintosh@gmail.com>
2019-09-11 13:57:33 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -27,12 +27,26 @@ end
 def play
   puts "Please enter a song name or number:"
   song_name = gets.strip
-  if song_name
+  if (1..9).to_a.include?(song_to_play.to_i)
+    puts "Playing #{songs[song_to_play.to_i - 1]}"song_name
     puts "Playing #{song_name}"
   else
     puts "Invalid input, please try again"
   end
 end
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  song_to_play = gets.chomp
+  if (1..9).to_a.include?(song_to_play.to_i)
+    puts "Playing #{songs[song_to_play.to_i - 1]}"
+  elsif songs.include?(song_to_play)
+    puts "Playing #{song_to_play}"
+  else
+    puts "Invalid input, please try again"
+  end
+end
+
 
 def exit_jukebox
   puts "Goodbye"
